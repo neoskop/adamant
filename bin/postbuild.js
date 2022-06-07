0d4ca63cb742b9e6be9c00ae913764dea7f1779f
Mark Wecke <wecke@neoskop.de>
2022-06-07 11:35:46 +0200

    fix: fix fesm2015 imports

--- bin/postbuild.js
+++ bin/postbuild.js
@@ -14,6 +14,12 @@ distPkg.main = cjsPath;
 distPkg.exports['.'].node = cjsPath;
 distPkg.exports['.'].require = cjsPath;
 
+let fesm2015Adamant = fs.readFileSync(__dirname + '/../dist/fesm2015/neoskop-adamant.mjs', 'utf8');
+
+fesm2015Adamant = fesm2015Adamant.replace(/import {(.+)} from 'tslib';/, "import * as __tslib from 'tslib';\nconst {$1} = __tslib;");
+fesm2015Adamant = fesm2015Adamant.replace(/import {(.+)} from 'rxjs';/, "import * as __rxjs from 'rxjs';\nconst {$1} = __rxjs;");
+
+fs.writeFileSync(__dirname + '/../dist/fesm2015/neoskop-adamant.mjs', fesm2015Adamant);
 fs.writeFileSync(__dirname + '/../dist/cjs/package.json', JSON.stringify({ module: 'commonjs' }, null, 2));
 fs.writeFileSync(__dirname + '/../dist/package.json', JSON.stringify(distPkg, null, 2));
 fs.writeFileSync(__dirname + '/../dist/README.md', fs.readFileSync(__dirname + '/../README.md'));
